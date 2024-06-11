8c6023362cd4ce0804aafa0ede55d3f50b596f64
prathmeshS18 <prathmeshshinde167@gmail.com>
2024-06-11 15:54:12 +0530

    practice

---
+++ variables.py
@@ -0,0 +1,25 @@
+variables
+name = "prathmesh"
+print(name)
+
+# x = "5"
+# print(x)
+
+# operators
+
+# x = 5
+# y = 11
+# z = x + y
+# print(z)
+
+# x = int(input("enter the  no. :"))
+# y = int(input("enter the  no. :"))
+# z = x / y
+# print(z)
+
+# type conversion
+
+# a, b = 1 ,"2"
+# c = int(b) #converted str val in int val
+# sum = a+c 
+# print(sum)
