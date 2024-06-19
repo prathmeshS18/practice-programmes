7af77f9d947d256b549f9e507ab2b4ee0b99b84b
prathmeshS18 <prathmeshshinde167@gmail.com>
2024-06-19 12:52:38 +0530

    creating init function

--- oops.py
+++ oops.py
@@ -1,5 +1,17 @@
-class Student:
-    name = "prathmesh"
-    id = 101
-s1 = Student()
-print(s1.name, s1.id)
+# creating class and objects
+
+# class Student:
+#     name = "prathmesh"
+#     id = 101
+# s1 = Student()
+# print(s1.name, s1.id)
+
+# creating init function
+class Stud:
+    def __init__(self,name):
+        self.name = name
+
+s1= Stud("prathmesh")
+print(s1.name)
+
+        
